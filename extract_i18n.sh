c36c00f23fb8ba19cbd83ab8237978d506051606
Milton Madanda <madandat@gmail.com>
2014-12-01 09:33:39 +0200

    update translation strings

--- extract_i18n.sh
+++ extract_i18n.sh
@@ -4,7 +4,7 @@ mkdir -p unicorecmsgem/locale
 
 pot-create -o unicorecmsgem/locale/unicorecmsgem.pot unicorecmsgem/
 
-declare -a arr=("eng_GB" "tha_TH" "ind_ID" "swa_TZ" "hin_IN")
+declare -a arr=("eng_GB" "tha_TH" "ind_ID" "swa_TZ" "hin_IN" "fre_FR")
 
 for lang in "${arr[@]}"
 do
