ae40dcd19fe8b12170223c7cb49fdb481838b43b
Milton Madanda <madandat@gmail.com>
2014-12-10 14:04:29 +0200

    rename init references

--- unicorecmsskeleton/__init__.py
+++ unicorecmsskeleton/__init__.py
@@ -5,10 +5,10 @@ def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
     config = Configurator(settings=settings)
-    config.add_translation_dirs('unicorecmsgem:locale')
+    config.add_translation_dirs('unicorecmsskeleton:locale')
     config.include('cms')
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.scan()
 
-    config.override_asset('cms:templates/', 'unicorecmsgem:templates/')
+    config.override_asset('cms:templates/', 'unicorecmsskeleton:templates/')
     return config.make_wsgi_app()
